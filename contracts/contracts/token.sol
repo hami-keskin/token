2dd671398628ab71f18597712514bb0525401643
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-08-01 20:41:48 +0300

    .

--- contracts/contracts/token.sol
+++ contracts/contracts/token.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract MyToken {
-    string public name = "KSK";
-    string public symbol = "KSK";
-    uint8 public decimals = 18;
+    string public name ;
+    string public symbol;
+    uint8 public decimals;
     uint256 private _totalSupply;
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
